f0e0cc4dfa16f8affdb5041669dbcf392886bf72
bswierzewski <swierzewski.bartosz@gmail.com>
2024-03-11 13:46:10 +0100

    Remove higlight.js

--- src/components/higlight/CodeHighlighter.tsx
+++ src/components/higlight/CodeHighlighter.tsx
@@ -1,25 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css';
-
 interface CodeHighlighterProps {
   code: string;
 }
 
 const CodeHighlighter: React.FC<CodeHighlighterProps> = ({ code }) => {
-  const codeRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    if (codeRef.current) {
-      codeRef.current.innerHTML = hljs.highlightAuto(code).value;
-    }
-  }, [code]);
-
   return (
     <pre>
-      <code ref={codeRef} className="hljs">
-        {/* Initial content will be replaced by highlighted code */}
-      </code>
+      <code>{code}</code>
     </pre>
   );
 };
