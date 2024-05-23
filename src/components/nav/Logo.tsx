4325e8c379f88685b24cdf0dc993d891f51f4de5
bswierzewski <swierzewski.bartosz@gmail.com>
2024-05-23 08:00:40 +0200

    Load logo based on theme after refresh page

--- src/components/nav/Logo.tsx
+++ src/components/nav/Logo.tsx
@@ -1,26 +1,32 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
-import Image from 'next/image';
-import React from 'react';
 import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useEffect, useMemo, useState } from 'react';
 
 export default function Logo() {
   const router = useRouter();
   const pathname = usePathname();
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  function doReset() {
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const doReset = () => {
     if (pathname !== '/') router.push('/');
-  }
+  };
+
+  const logoSrc = useMemo(() => {
+    if (!mounted) return '/default-logo.svg'; // Default or placeholder logo
+    return theme === 'dark' ? '/logo-dark.svg' : '/logo-light.svg';
+  }, [theme, mounted]);
 
   return (
     <div onClick={doReset} className="cursor-pointer flex items-center gap-2 text-3xl font-semibold">
-      {theme == 'dark' ? (
-        <Image src="/logo-dark.svg" height={120} width={120} alt={'logo'} />
-      ) : (
-        <Image src="/logo-light.svg" height={120} width={120} alt={'logo'} />
-      )}
+      <Image src={logoSrc} height={120} width={120} alt={mounted ? `${theme} theme logo` : 'logo'} />
     </div>
   );
 }
